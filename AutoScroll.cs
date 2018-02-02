47d3c4f9e046c0d810f9c6f38b1e271a2e63f54b
bidaningu <32624339+bidaningu@users.noreply.github.com>
2018-02-02 23:06:09 +0900

    Add files via upload

---
+++ AutoScroll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class AutoScroll : MonoBehaviour {
+	
+	public RectTransform userText;
+	private float scrollSpeed = 1f;
+	private Vector3 textPosition;
+
+	private void Update() {
+		Debug.Log("textPosition.y : " + userText.anchoredPosition);
+	}
+
+	void Awake() {
+		userText = GetComponent<RectTransform>();
+	}
+	public void ScrollText()
+	{
+		textPosition = userText.anchoredPosition;
+		for(float i = 0; i < 100; i ++)
+		{
+			textPosition.y += scrollSpeed;
+		}
+	}
+}
